aa23d603ce1b0613c03feb3ea01f8b0a2c931b38
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-01-26 01:58:34 +0800

    finish part3 3.19.-3.21.

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -35,7 +37,7 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -44,14 +46,23 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (Person.findOne({ name: body.name })) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -75,14 +86,13 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.put("/api/persons/:id", (request, response) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
